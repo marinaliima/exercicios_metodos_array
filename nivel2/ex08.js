fed0566bfdcd75151f0e6c492f9f19385a62ea56
marinaliima <marina.l@edu.senai.br>
2026-08-28 20:56:49 -0300

    feat: adiciona exercício 8, do nível 2

--- nivel2/ex08.js
+++ nivel2/ex08.js
@@ -1,2 +1,7 @@
 //8. slice() não altera o original
 
+let planetas = ["Marte", "Terra", "Netuno", "Júpiter", "Urano"];
+let planetasNovos = planetas.slice(2, 5);
+
+console.log("Array original:", planetas);
+console.log("Array criado:", planetasNovos);
